0e2fb901a871eb4d5e86522590feefbb4061638d
Jitindra Fartiyal <jayfartiyal@hotmail.com>
2016-09-26 10:35:58 +0000

    added updated file in master

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
 var express = require ('express');
 var bodyParser = require ('body-parser');
+var path = require('path');
+require('./node_modules/angular/controller')(App)
 
 var app = express();
 
 // configire the app
 
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,"node_modules")));
+app.use(express.static(path.join(__dirname,'views')));
+
 // use middlewares
 
+app.use(bodyParser.json());
+
 // define routes
+
+app.get('/contactlist',function(req,res){
+    
+    console.log('Request for Contact List');
+    var person1 = {
+        Name : "Jay Fartiyal",
+        Email : "jayfartiyal@hotmail.com",
+        Phone : "9886762064"
+    };
+    
+    var person2 = {
+        Name : "Preethi K A",
+        Email : "preethiKA@hotmail.com",
+        Phone : "9972232062"
+    };
+    
+    var person3 = {
+        Name : "Utkarsh Roy",
+        Email : "utkarshRoy@hotmail.com",
+        Phone : "9878965002"
+    };
+    
+    var contactList = [person1,person2,person3];
+    
+    res.json(contactList);
+});
 
 // start the server
 
